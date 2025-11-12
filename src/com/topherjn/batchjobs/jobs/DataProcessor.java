690fc4a5cae79d5ddc103c5e8af4a99567e21253
topherjn <nicholsonc@gmail.com>
2025-11-12 10:03:58 -0500

    DataProcessor class added

--- src/com/topherjn/batchjobs/jobs/DataProcessor.java
+++ src/com/topherjn/batchjobs/jobs/DataProcessor.java
@@ -1,4 +1,27 @@
 package com.topherjn.batchjobs.jobs;
 
-public class DataProcessor {
+import java.io.File;
+import java.io.IOException;
+
+public abstract class DataProcessor {
+
+    private final File inputFile;
+    private final File outputFile;
+
+    protected DataProcessor(String inputFilename, String outputFilename) {
+        if (inputFilename == null || outputFilename == null) {
+            throw new IllegalArgumentException("Filenames cannot be null");
+        }
+        this.inputFile = new File(inputFilename);
+        this.outputFile = new File(outputFilename);
+    }
+
+    public abstract void process() throws IOException;
+
+    public File getInputFile() {
+        return inputFile;
+    }
+    public File getOutputFile() {
+        return outputFile;
+    }
 }

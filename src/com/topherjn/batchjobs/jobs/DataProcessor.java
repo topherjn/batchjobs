b33aaef4f8f599a5eeba8246e933e189d5630127
topherjn <nicholsonc@gmail.com>
2025-11-12 11:59:10 -0500

    DataProcessor refactored

--- src/com/topherjn/batchjobs/jobs/DataProcessor.java
+++ src/com/topherjn/batchjobs/jobs/DataProcessor.java
@@ -1,8 +1,13 @@
 package com.topherjn.batchjobs.jobs;
 
+import com.topherjn.batchjobs.visitor.JobVisitor;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The abstract base class for all processing jobs.
+ * It is modified to include the 'accept' method for the Visitor pattern.
+ */
 public abstract class DataProcessor {
 
     private final File inputFile;
@@ -16,8 +21,21 @@ public abstract class DataProcessor {
         this.outputFile = new File(outputFilename);
     }
 
+    /**
+     * The job's core processing logic. (Unchanged)
+     */
     public abstract void process() throws IOException;
 
+    /**
+     * NEW: The 'accept' method for the Visitor Pattern.
+     * This forces all concrete subclasses to implement this method,
+     * which enables the "double dispatch" mechanism.
+     *
+     * @param visitor The visitor that will operate on this job.
+     */
+    public abstract void accept(JobVisitor visitor);
+
+    // --- Getters (Unchanged) ---
     public File getInputFile() {
         return inputFile;
     }
